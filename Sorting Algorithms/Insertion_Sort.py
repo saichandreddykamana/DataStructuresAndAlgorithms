8630227ea1958df6eeff57b6eb5059047299d9c4
Sai Chand Reddy Kamana <saichand.kamana@outlook.com>
2022-11-24 18:32:44 +0000

    Update Insertion_Sort.py

--- Sorting Algorithms/Insertion_Sort.py
+++ Sorting Algorithms/Insertion_Sort.py
@@ -1,20 +1,23 @@
 # Insertion Sort
 def insertion_sort(arr):
-    swap = False
+    swapped = False
     for i in range(0, len(arr)):
         if i + 1 < len(arr):
-            temp = arr[i]
             if arr[i+1] < arr[i]:
-                arr[i] = arr[i+1]
-                arr[i+1] = temp
-                swap = True
-            if swap == True:
+                arr = swap(i, i+1, arr)
+                swapped = True
+            if swapped == True:
                 for j in reversed(range(0, i)):
                     if j - 1 >= 0:
                         if arr[j - 1] > arr[j]:
-                            temp = arr[j]
-                            arr[j] = arr[j - 1]
-                            arr[j - 1] = temp
+                            arr = swap(j, j-1, arr)
+    return arr
+
+
+def swap(current_index,next_index, arr):
+    temp = arr[current_index]
+    arr[current_index] = arr[next_index]
+    arr[next_index] = temp
     return arr
 
     
